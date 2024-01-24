df0c70fabc18daa2e6d3d72545a099d1bfad7eb9
Athirson Silva <athirson.silva@proton.me>
2024-01-24 20:10:57 -0300

    handler functions

--- main.go
+++ main.go
@@ -1,30 +1,59 @@
 package main
 
 import (
+	"errors"
+	"fmt"
 	"log"
 	"net/http"
 )
 
+const port = ":8080"
+
+func Home(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodGet {
+		w.WriteHeader(http.StatusMethodNotAllowed)
+		return
+	}
+
+	w.Header().Set("Content-Type", "text/plain")
+	w.Write([]byte("Hello world!"))
+}
+
+func About(w http.ResponseWriter, r *http.Request) {
+	w.Write(([]byte("About page")))
+}
+
+func NotFound(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusNotFound)
+	w.Write([]byte("Page not found"))
+}
+
+func Divide(w http.ResponseWriter, r *http.Request) {
+	var f, err = calculateDivide(10, 2)
+
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte(err.Error()))
+		return
+	}
+
+	w.Write([]byte(fmt.Sprintf("10 / 2 = %v", f)))
+}
+
+func calculateDivide(a, b int) (int, error) {
+	if b == 0 || a == 0 {
+		return 0, errors.New("cannot divide by zero")
+	}
+
+	return a / b, nil
+}
+
 func main() {
-	log.Print("Starting the application...")
+	http.HandleFunc("/", Home)
+	http.HandleFunc("/about", About)
+	http.HandleFunc("/not-found", NotFound)
+	http.HandleFunc("/divide", Divide)
 
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		switch r.Method {
-		case "GET":
-			log.Print("GET request")
-		case "POST":
-			log.Print("POST request")
-		case "PUT":
-			log.Print("PUT request")
-		case "DELETE":
-			log.Print("DELETE request")
-		default:
-			log.Print("Unknown request")
-		}
-
-		log.Print(r.Header, r.Body, r.URL.Path)
-		w.Write([]byte("Hello world!"))
-	})
-
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	log.Printf("Starting the server on port %v\n", port)
+	log.Fatal(http.ListenAndServe(port, nil))
 }
